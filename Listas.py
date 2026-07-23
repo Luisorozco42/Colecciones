70b8617558c00b061d6139485bed51925927699e
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-22 20:29:05 -0600

    learning about lists

---
+++ Listas.py
@@ -0,0 +1,12 @@
+# iniciamos con listas
+print("*** Manejo de Listas ***")
+
+mi_lista = [1, 2, 3, 4, 5]
+print(f'{mi_lista} -> Lista original')
+
+# largo de una lista
+print(f"Largo de la lista: {len(mi_lista)}")
+
+# Acceder a los elementos de la lista por indice
+print(f"Accedemos al valor del indice 4: {mi_lista[4]}")
+print(f"Accedemos al último indics de la lista: {mi_lista[-1]}")

05c3c50d875c839aae8cd8bda62831e924c81825
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-23 21:35:56 -0600

    learning how to print every item on a list

---
+++ videos/IterarListas.py
@@ -0,0 +1,13 @@
+print(f"*** Iterar Listas ***")
+
+nombres = ['Karla', 'Juan', 'Laura']
+
+for nombre in nombres:
+    print(nombre)
+
+# Bueno, ya veo que se pasa por donde quiere los tipos de datos
+lista_heterogenea = [100, True, 'Ivonne']
+
+print()
+for elemento in lista_heterogenea:
+    print(elemento)
